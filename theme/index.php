2deaa202c50411b5b80d289959fd3c8c75e5a4d1
gordondalos <gordondalos@gmail.com>
2015-12-29 22:16:52 +0600

    сделал шаблон

---
+++ theme/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]>
+<html class="ie ie6" lang="en-US"> <![endif]-->
+<!--[if IE 7 ]>
+<html class="ie ie7" lang="en-US"> <![endif]-->
+<!--[if IE 8 ]>
+<html class="ie ie8" lang="en-US"> <![endif]-->
+<!--[if IE 9 ]>
+<html class="ie ie9" lang="en-US"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en-US"> <!--<![endif]-->
+
+<?php include "./parts/head.php"?>
+
+<body class="home page page-id-203 page-template page-template-page-home-php">
+<div id="motopress-main" class="main-holder">
+
+    <?php include "./parts/header.php"?>
+
+
+    <div class="motopress-wrapper content-holder clearfix">
+
+        <?php include "./parts/up_slider.php"?>
+
+
+        <div class="container">
+            <div class="row">
+                <?php include "./parts/main_item.php"?>
+                <?php include "./parts/latest_project.php"?>
+
+
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="newsletter-widget-wrap">
+        <div class="container">
+            <div class="row">
+
+                <?php include "./parts/search_bar.php"?>
+
+            </div>
+        </div>
+    </div>
+
+    <?php include "./parts/footer.php"?>
+
+</div>
+<div id="back-top-wrapper" class="visible-desktop">
+    <p id="back-top">
+        <a href="#top"><span></span></a></p>
+</div>
+
+<?php include "./parts/js.php"?>
+
+
+
+</body>
+</html>
