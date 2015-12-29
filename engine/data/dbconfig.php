cf894ae19203d8706b954530c5bd78cb23d5e131
gordondalos <gordondalos@gmail.com>
2015-12-29 19:42:41 +0600

    развернул, начинаю делать шаблон

---
+++ engine/data/dbconfig.php
@@ -0,0 +1,21 @@
+<?PHP
+
+define ("DBHOST", "localhost");
+
+define ("DBNAME", "greensoft");
+
+define ("DBUSER", "root");
+
+define ("DBPASS", "");
+
+define ("PREFIX", "dle");
+
+define ("USERPREFIX", "dle");
+
+define ("COLLATE", "utf8");
+
+define('SECURE_AUTH_KEY', '`MOGx.OYgeXmed]]3BFN63TT2^Y?e%8+5KO7XE/=mPv08a3*H][^/L18@S,uPn9c');
+
+$db = new db;
+
+?>
---
+++ engine/data/videoconfig.php
@@ -0,0 +1,31 @@
+<?PHP
+
+//Videoplayers Configurations
+
+$video_config = array (
+
+'width' => "425",
+
+'height' => "325",
+
+'play' => "false",
+
+'progressBarColor' => "0xFFFFFF",
+
+'flv_watermark' => "1",
+
+'tube_related' => "0",
+
+'tube_dle' => "0",
+
+'flv_watermark_pos' => "left",
+
+'flv_watermark_al' => "1",
+
+'audio_width' => "425",
+
+'preload' => '1',
+
+);
+
+?>
